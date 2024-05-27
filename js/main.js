6ee72156895e8f31d25950ba260d1d8fc770702d
DogiFnf <dogifnf@gmail.com>
2024-05-27 14:44:17 +0300

    Первая версия сайта

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.getElementById('aboutButton').addEventListener('click', function() {
+    window.location.href = 'about.html';
+});
+
+document.getElementById('contactsButton').addEventListener('click', function() {
+    window.location.href = 'contacts.html';
+});
+
+
+document.getElementById('copyButton').addEventListener('click', function() {
+    var textToCopy = this.getAttribute('data-text');
+    var tempInput = document.createElement('input');
+    tempInput.value = textToCopy;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert('Текст скопирован в буфер обмена!');
+});
